c90beb74732265494250221162d8c08fafe05d2e
Abin Sunny <105172510+mrabinsunny@users.noreply.github.com>
2024-06-15 12:05:15 +0530

    welcomescripts.js

--- welcomescripts.js
+++ welcomescripts.js
@@ -1,6 +1,6 @@
-function redirectToAbout() {
-    document.body.classList.add("fade-out");
+document.getElementById("s6cseaButton").addEventListener("click", function() {
+    document.getElementById("s6cseaButton").classList.add("fade-out");
     setTimeout(function() {
-        window.location.href = "about.html";
-    }, 800);
-}
+        window.location.href = "facetrack.html";
+    }, 800); // Redirect after 0.5s (500ms) fade-out effect
+});
